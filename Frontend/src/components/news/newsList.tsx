683a30b2ad1fb4dd79d13b7da7a70ca1b5a46e92
gryzzlycruise <gryzzlycruise@gmail.com>
2022-10-10 21:24:20 +0300

    feat: added newsPage pagination

--- Frontend/src/components/news/newsList.tsx
+++ Frontend/src/components/news/newsList.tsx
@@ -4,27 +4,46 @@ import axios from "axios";
 import {NewsItem} from "./newsItem";
 import {PropsNewsItem} from "./newsItem";
 
-export const NewsList = () => {
-    const [articles, setArticles] = useState([]);
+interface PropsNewsList {
+    fetching: boolean,
+    setFetching: Function
+}
+
+type Article = {
+    title: string,
+    description: string,
+    url: string,
+    urlToImage: string,
+    publishedAt: string
+}
+
+export const NewsList = ({fetching, setFetching}: PropsNewsList) => {
+    const [articles, setArticles] = useState<Article[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
-        const getArticles = async () => {
-            const response = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&language=en&apiKey=b5f970c697094202927aa40adb3dcdad');
-            setArticles(response.data.articles);
+        if (fetching) {
+            axios.get(`https://newsapi.org/v2/everything?domains=wsj.com&language=en&pageSize=5&page=${currentPage}&apiKey=b5f970c697094202927aa40adb3dcdad`)
+                 .then(response => {
+                     setArticles([...articles, ...response.data.articles]);
+                     setCurrentPage(prevState => prevState + 1);
+                 })
+                .finally(() => setFetching(false));
         }
-
-        getArticles();
-    }, []);
+    }, [fetching]);
 
     return (
         <div>
-            {articles.map((article: PropsNewsItem) => {
-                return <NewsItem title={article.title}
-                                 description={article.description}
-                                 url={article.url}
-                                 urlToImage={article.urlToImage}
-                                 publishedAt={article.publishedAt}
-                        />
+            {articles.map((article: PropsNewsItem, index: number) => {
+                return <div key={index}>
+                    <NewsItem title={article.title}
+                              description={article.description}
+                              url={article.url}
+                              urlToImage={article.urlToImage}
+                              publishedAt={article.publishedAt}
+                    />
+                </div>
+
             })}
         </div>
     )
--- Frontend/src/pages/newsPage.tsx
+++ Frontend/src/pages/newsPage.tsx
@@ -1,13 +1,33 @@
 import {NewsList} from "../components/news/newsList";
 
 import styles from "./newsPage.module.scss"
+import {useEffect, useState} from "react";
 
 export const NewsPage = () => {
+    const [fetching, setFetching] = useState(true);
+
+    useEffect(() => {
+        const newsList = document.getElementById('newsList') as HTMLElement;
+        newsList.addEventListener('scroll', scrollHandler);
+
+        return function() {
+            newsList.removeEventListener('scroll', scrollHandler);
+        }
+    }, []);
+
+    const scrollHandler = (event: any) => {
+        if (event.target.scrollHeight - (event.target.scrollTop + window.innerHeight) < 100) {
+            setFetching(true);
+        }
+    }
+
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.heading}>Latest news</h1>
-            <div className={styles.list}>
-                <NewsList/>
+            <div className={styles.list} id={'newsList'}>
+                <NewsList fetching={fetching}
+                          setFetching={setFetching}
+                />
             </div>
         </div>
     );
