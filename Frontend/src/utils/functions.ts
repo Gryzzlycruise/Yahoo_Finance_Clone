ba9acf226cd14923af96c7a496762be6dc3e1914
gryzzlycruise <gryzzlycruise@gmail.com>
2022-10-14 20:45:41 +0300

    fix: changed date divider in changeDate function

--- Frontend/src/utils/functions.ts
+++ Frontend/src/utils/functions.ts
@@ -1,7 +1,7 @@
 export const changePublishTime = (publishAt: string): string => {
     const changedDate = publishAt.replace(/[A-Z]/g, ' ')
                                  .split(' ');
-    changedDate[0] = changedDate[0].split('-').reverse().join('-');
+    changedDate[0] = changedDate[0].split('-').reverse().join('.');
     return changedDate.reverse()
                       .join(' ')
                       .trim();
